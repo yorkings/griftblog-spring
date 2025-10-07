b76f0dc883daba874032a33dcb6c51eb40c43538
yorkings <yorkingsnyakundi2700@gmail.com>
2025-10-07 16:50:41 +0300

    started the application

---
+++ griftblog/src/main/java/com/example/griftblog/GriftblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.griftblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriftblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GriftblogApplication.class, args);
+	}
+
+}
---
+++ griftblog/src/main/java/com/example/griftblog/models/User.java
@@ -0,0 +1,27 @@
+package com.example.griftblog.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String  email;
+
+    private List<Role>roles=ne
+
+}
